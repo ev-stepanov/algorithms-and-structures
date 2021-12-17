b1979dcb3cc8ead1f75b004f3d3dc3c2254abac0
evgeniy stepanov <estepanov@neoflex.ru>
2021-12-17 14:54:30 +0400

    Implemented task 2.1

---
+++ Task2.1/src/main/java/Solution.java
@@ -0,0 +1,52 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    private static final Map<Character, Character> pairs = Map.of(
+        '(', ')',
+        '{', '}',
+        '[', ']'
+    );
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            Stack<Bracket> stack = new Stack<>();
+            char[] chars = reader.readLine().toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (pairs.containsKey(chars[i])) {
+                    stack.add(new Bracket(chars[i], i + 1));
+                } else {
+                    if (!pairs.containsValue(chars[i]))
+                        continue;
+                    if (stack.isEmpty() || !pairs.get(stack.peek().ch).equals(chars[i])) {
+                        System.out.println(i + 1);
+                        return;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            System.out.println(stack.isEmpty() ? "SUCCESS" : stack.pop().index);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Bracket {
+        private final char ch;
+        private final int index;
+
+        public Bracket(char ch, int index) {
+            this.ch = ch;
+            this.index = index;
+        }
+
+
+    }
+}
