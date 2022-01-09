f62da65d23c5f61ce904f09ee0e1624d4bb7c149
evgeniy stepanov <estepanov@neoflex.ru>
2022-01-09 11:24:22 +0400

    Implemented task 2.2

---
+++ Task2.2/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            Map<Integer, List<Integer>> mapOfVertices = createMapOfVertices(bufferedReader);
+            System.out.println(getHeight(mapOfVertices, -1) - 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int getHeight(Map<Integer, List<Integer>> mapOfVertices, int r) {
+        AtomicInteger height = new AtomicInteger(1);
+        if (!mapOfVertices.containsKey(r)) {
+            return height.get();
+        }
+
+        mapOfVertices.get(r)
+            .forEach(i -> height.set(Math.max(height.get(), getHeight(mapOfVertices, i) + 1)));
+        return height.get();
+    }
+
+    private static Map<Integer, List<Integer>> createMapOfVertices(BufferedReader bufferedReader) throws IOException {
+        bufferedReader.readLine();
+
+        return Arrays.stream(bufferedReader.readLine().split(" "))
+            .map(Integer::parseInt)
+            .map(Pair::new)
+            .collect(Collectors.groupingBy(a -> a.vertex,
+                Collectors.mapping(a -> Pair.index, Collectors.toList())));
+    }
+
+    private static class Pair {
+        private static Integer index = -1;
+        private final Integer vertex;
+
+        public Pair(Integer vertex) {
+            this.vertex = vertex;
+            index++;
+        }
+    }
+}
