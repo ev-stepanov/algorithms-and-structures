0de0fec6cc4ca6fe907b3d96627796c971ef77a8
evgeniy stepanov <estepanov@neoflex.ru>
2022-02-01 22:04:34 +0400

    refactoring task 2.1

--- Task2.1/src/main/java/Solution.java
+++ Task2.1/src/main/java/Solution.java
@@ -3,8 +3,9 @@ package main.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Map;
-import java.util.Stack;
 
 public class Solution {
     private static final Map<Character, Character> pairs = Map.of(
@@ -15,7 +16,7 @@ public class Solution {
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            Stack<Bracket> stack = new Stack<>();
+            Deque<Bracket> stack = new ArrayDeque<>();
             char[] chars = reader.readLine().toCharArray();
             for (int i = 0; i < chars.length; i++) {
                 if (pairs.containsKey(chars[i])) {
@@ -38,7 +39,7 @@ public class Solution {
         }
     }
 
-    public static class Bracket {
+    private static class Bracket {
         private final char ch;
         private final int index;
 
